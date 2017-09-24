9666c2d5ccd9fd9cc88d3d64032ee1570e12a1e7
Michele Ancheta <michelee.ancheta@gmail.com>
2017-09-23 21:51:55 -0400

    Up and running with Vue

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log('It works.');
+new Vue({
+	el: '#app',
+	data: {
+		total: 0,
+		items: [{ title: 'prueba 1'}, { title: 'prueba 2'}, { title: 'prueba 3'}],
+	},
+	methods:{
+		addItem:function(){
+			this.total += 9.99;
+		}
+	}
+});
